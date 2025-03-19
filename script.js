bc76994b5175963f3a78abc69ddf2958cb7efee2
vikrantsrgc <145971065+vikrantsrgc@users.noreply.github.com>
2025-03-19 17:39:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-const byteSize = (str) => {
-  // write your code here
-};
+ function byteSize(str) {
+    // Create a TextEncoder instance
+    const encoder = new TextEncoder();
+    
+    // Encode the string into a Uint8Array of UTF-8 bytes
+    const encoded = encoder.encode(str);
+    
+    // Return the length of the Uint8Array, which is the size in bytes
+    return encoded.length;
+} 
 
 // Do not change the code below
 const str = prompt("Enter some string.");
